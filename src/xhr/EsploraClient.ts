fcf94e8a1956d76ef5ec645514497d4292252dc5
Penguen Umut <uuisik@gmail.com>
2021-12-13 23:04:44 +0300

    EsploraClient get types added

--- src/xhr/EsploraClient.ts
+++ src/xhr/EsploraClient.ts
@@ -9,27 +9,27 @@ export class EsploraClient {
     this.axios.defaults.baseURL = esploraApiUrl;
   }
 
-  blocks = async (): Promise<Block[]> => this.axios.get("blocks").then<Block[]>((response) => response.data);
+  blocks = async (): Promise<Block[]> => this.axios.get<Block[]>("blocks").then<Block[]>((response) => response.data);
 
-  block = async (blockHash: string): Promise<Block> => this.axios.get(`block/${blockHash}`).then<Block>((response) => response.data);
+  block = async (blockHash: string): Promise<Block> => this.axios.get<Block>(`block/${blockHash}`).then<Block>((response) => response.data);
 
-  blockStatus = async (blockHash: string): Promise<BlockStatus> => this.axios.get(`block/${blockHash}/status`).then<BlockStatus>((response) => response.data);
+  blockStatus = async (blockHash: string): Promise<BlockStatus> => this.axios.get<BlockStatus>(`block/${blockHash}/status`).then<BlockStatus>((response) => response.data);
 
-  blockTxs = async (blockHash: string): Promise<TxDetail[]> => this.axios.get(`block/${blockHash}/txs/0`).then<TxDetail[]>((response) => response.data);
+  blockTxs = async (blockHash: string): Promise<TxDetail[]> => this.axios.get<TxDetail[]>(`block/${blockHash}/txs/0`).then<TxDetail[]>((response) => response.data);
 
-  blockTipHeight = async (): Promise<BlockTipHeight> => this.axios.get("blocks/tip/height").then<BlockTipHeight>((response) => response.data);
+  blockTipHeight = async (): Promise<BlockTipHeight> => this.axios.get<BlockTipHeight>("blocks/tip/height").then<BlockTipHeight>((response) => response.data);
 
-  txs = async (): Promise<Tx[]> => this.axios.get("mempool/recent").then<Tx[]>((response) => response.data);
+  txs = async (): Promise<Tx[]> => this.axios.get<Tx[]>("mempool/recent").then<Tx[]>((response) => response.data);
 
-  tx = async (txid: string): Promise<TxDetail> => this.axios.get(`tx/${txid}`).then<TxDetail>((response) => response.data);
+  tx = async (txid: string): Promise<TxDetail> => this.axios.get<TxDetail>(`tx/${txid}`).then<TxDetail>((response) => response.data);
 
-  txOutspends = async (txid: string): Promise<TxOutSpend[]> => this.axios.get(`tx/${txid}/outspends`).then<TxOutSpend[]>((response) => response.data);
+  txOutspends = async (txid: string): Promise<TxOutSpend[]> => this.axios.get<TxOutSpend[]>(`tx/${txid}/outspends`).then<TxOutSpend[]>((response) => response.data);
 
-  asset = async (assetId: string): Promise<Asset> => this.axios.get(`asset/${assetId}`).then<Asset>((response) => response.data);
+  asset = async (assetId: string): Promise<Asset> => this.axios.get<Asset>(`asset/${assetId}`).then<Asset>((response) => response.data);
 
-  assetTx = async (assetId: string): Promise<TxDetail[]> => this.axios.get(`asset/${assetId}/txs`).then<TxDetail[]>((response) => response.data);
+  assetTx = async (assetId: string): Promise<TxDetail[]> => this.axios.get<TxDetail[]>(`asset/${assetId}/txs`).then<TxDetail[]>((response) => response.data);
 
-  mempool = async (): Promise<Mempool> => this.axios.get("mempool").then<Mempool>((response) => response.data);
+  mempool = async (): Promise<Mempool> => this.axios.get<Mempool>("mempool").then<Mempool>((response) => response.data);
 
-  feeEstimates = async (): Promise<any> => this.axios.get("fee-estimates").then<any>((response) => response.data);
+  feeEstimates = async (): Promise<any> => this.axios.get<any>("fee-estimates").then<any>((response) => response.data);
 }
