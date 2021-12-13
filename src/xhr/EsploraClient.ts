5b08d765dd9fbc1ceedd55665c38591a12948848
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-13 19:10:29 +0300

    EsploraClient: txs, tx get requests

--- src/xhr/EsploraClient.ts
+++ src/xhr/EsploraClient.ts
@@ -1,5 +1,5 @@
 import axiosLib, { AxiosInstance } from "axios";
-import { Block } from "..";
+import { Block, TxDetail, Tx } from "..";
 
 export class EsploraClient {
   private axios: AxiosInstance;
@@ -12,4 +12,8 @@ export class EsploraClient {
   blocks = async (): Promise<Block[]> => this.axios.get("blocks").then<Block[]>((response) => response.data);
 
   block = async (blockHash: string): Promise<Block> => this.axios.get(`block/${blockHash}`).then<Block>((response) => response.data);
+
+  txs = async (): Promise<Tx[]> => this.axios.get("mempool/recent").then<Tx[]>((response) => response.data);
+
+  tx = async (txid: string): Promise<TxDetail> => this.axios.get(`tx/${txid}`).then<TxDetail>((response) => response.data);
 }
