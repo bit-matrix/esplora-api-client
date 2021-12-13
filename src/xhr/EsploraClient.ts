832ec60aac19a71cb420e6cf0e43461b746574c0
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-13 20:04:19 +0300

    EsploraClient: blockStatus, blockTxs,blockTipHeight requests added.

--- src/xhr/EsploraClient.ts
+++ src/xhr/EsploraClient.ts
@@ -1,5 +1,5 @@
 import axiosLib, { AxiosInstance } from "axios";
-import { Block, TxDetail, Tx } from "..";
+import { Block, TxDetail, Tx, BlockStatus, BlockTipHeight } from "..";
 
 export class EsploraClient {
   private axios: AxiosInstance;
@@ -12,6 +12,12 @@ export class EsploraClient {
   blocks = async (): Promise<Block[]> => this.axios.get("blocks").then<Block[]>((response) => response.data);
 
   block = async (blockHash: string): Promise<Block> => this.axios.get(`block/${blockHash}`).then<Block>((response) => response.data);
+
+  blockStatus = async (blockHash: string): Promise<BlockStatus> => this.axios.get(`block/${blockHash}/status`).then<BlockStatus>((response) => response.data);
+
+  blockTxs = async (blockHash: string): Promise<TxDetail[]> => this.axios.get(`block/${blockHash}/txs/0`).then<TxDetail[]>((response) => response.data);
+
+  blockTipHeight = async (): Promise<BlockTipHeight> => this.axios.get("blocks/tip/height").then<BlockTipHeight>((response) => response.data);
 
   txs = async (): Promise<Tx[]> => this.axios.get("mempool/recent").then<Tx[]>((response) => response.data);
 
